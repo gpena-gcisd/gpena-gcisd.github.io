7d5cda33fcae6e7cd3763da3addf0704e95cb3e7
gpena-gcisd <115046828+gpena-gcisd@users.noreply.github.com>
2022-10-28 19:49:37 -0500

    Create if.js
    it's as shrimple as that

---
+++ u5/u5_2/if.js
@@ -0,0 +1,15 @@
+let age = 20;
+if (age >= 18) {
+    console.log("You are old enough to vote!");
+}
+console.log("Thank you for checking your age!");
+//////////////////////////////////////////////
+let compSciGrade = 95
+if (compSciGrade >= 90) {
+    console.log("You can go to the mall!");
+}
+//////////////////////////////////////////////
+age = 15;
+if (age < 16) {
+    console.log("Sorry, you are not old enough to drive.");
+}
